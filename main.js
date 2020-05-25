4e050a6a474f65db27f50d1750540c3fb9c63f35
Rene <kirenester@gmail.com>
2020-05-25 10:50:46 +0000

    callback function instead of setTimeout()

--- main.js
+++ main.js
@@ -1,27 +1,49 @@
-var xhr = new XMLHttpRequest();
-var data;
+// var xhr = new XMLHttpRequest();
+// var data;
 
-xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
-xhr.send();
+// xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
+// xhr.send();
 
-// function setData (jsonData) {
-//     data = jsonData;
-// }
+// // function setData (jsonData) {
+// //     data = jsonData;
+// // }
 
-xhr.onreadystatechange = function() {
-    //console.log(this.readyState);
-    if (this.readyState == 4 && this.status == 200) {
-        //document.getElementById("data").innerHTML = this.responseText;
-        //console.log(typeof(this.responseText));
-        //console.log(typeof(JSON.parse(this.responseText)));
-        //console.log(JSON.parse(this.responseText));
-        //data = this.responseText;
-        //console.log(data);
-        //setData(JSON.parse(this.responseText));
-        data = JSON.parse(this.responseText);
-    }
-};
+// xhr.onreadystatechange = function() {
+//     //console.log(this.readyState);
+//     if (this.readyState == 4 && this.status == 200) {
+//         //document.getElementById("data").innerHTML = this.responseText;
+//         //console.log(typeof(this.responseText));
+//         //console.log(typeof(JSON.parse(this.responseText)));
+//         //console.log(JSON.parse(this.responseText));
+//         //data = this.responseText;
+//         //console.log(data);
+//         //setData(JSON.parse(this.responseText));
+//         data = JSON.parse(this.responseText);
+//     }
+// };
 
-setTimeout(function(){
+// setTimeout(function(){
+//     console.log(data);
+// }, 500);
+
+function getData(cb) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
+    xhr.send();
+
+    xhr.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+            cb(JSON.parse(this.responseText));
+        }
+   };
+}
+
+function printDataToconsole(data) {
     console.log(data);
-}, 500);
+}
+
+// getData(function(data) {
+//     console.log(data);
+// })
+
+getData(printDataToconsole);
