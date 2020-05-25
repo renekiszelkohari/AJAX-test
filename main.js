25be9b32326f430978ba86b1d3dfcc7be49d5637
Rene <kirenester@gmail.com>
2020-05-25 10:36:07 +0000

    use setTimeout() to solve data displaying problem

--- main.js
+++ main.js
@@ -1,10 +1,27 @@
 var xhr = new XMLHttpRequest();
+var data;
 
 xhr.open("GET", "https://ci-swapi.herokuapp.com/api/");
 xhr.send();
 
+// function setData (jsonData) {
+//     data = jsonData;
+// }
+
 xhr.onreadystatechange = function() {
+    //console.log(this.readyState);
     if (this.readyState == 4 && this.status == 200) {
-        document.getElementById("data").innerHTML = this.responseText;
+        //document.getElementById("data").innerHTML = this.responseText;
+        //console.log(typeof(this.responseText));
+        //console.log(typeof(JSON.parse(this.responseText)));
+        //console.log(JSON.parse(this.responseText));
+        //data = this.responseText;
+        //console.log(data);
+        //setData(JSON.parse(this.responseText));
+        data = JSON.parse(this.responseText);
     }
 };
+
+setTimeout(function(){
+    console.log(data);
+}, 500);
