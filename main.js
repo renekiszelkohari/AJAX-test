f3d52d58359498bcfc1c69a6c3f617e4bec22a27
Rene <kirenester@gmail.com>
2020-05-28 10:51:05 +0000

    implement pagination, changed button url in HTML

--- main.js
+++ main.js
@@ -100,18 +100,17 @@
 // }
 
 // ------------- TABULAR DATA ----------
-const baseUrl = "https://ci-swapi.herokuapp.com/api/"
-
-function getData(type, cb) {
+function getData(url, cb) {
      var xhr = new XMLHttpRequest();
-     xhr.open("GET", baseUrl + type + "/");
-     xhr.send();
 
      xhr.onreadystatechange = function() {
          if (this.readyState == 4 && this.status == 200) {
              cb(JSON.parse(this.responseText));
          }
     };
+
+     xhr.open("GET", url);
+     xhr.send();
 }
 
 function getTableHeaders(obj) {
@@ -123,12 +122,26 @@ function getTableHeaders(obj) {
     return `<tr>${tableHeaders}</tr>`
 }
 
-function writeToDocument(type) {
+function generatePaginationButtons(next, prev) {
+    if (next && prev) {
+        return `<button onclick="writeToDocument('${prev}')">Previous</button><button onclick="writeToDocument('${next}')">Next</button>`
+    } else if (next && !prev) {
+        return `<button onclick="writeToDocument('${next}')">Next</button>`
+    } else if (!next && prev) {
+        `<button onclick="writeToDocument('${prev}')">Previous</button>`
+    }
+}
+
+function writeToDocument(url) {
     var tableRows = [];
     var el = document.getElementById("data");
     el.innerHTML = "";
     
-    getData(type, function(data){
+    getData(url, function(data){
+        var pagination = "";
+        if (data.next || data.previous) {
+            pagination = generatePaginationButtons(data.next, data.previous)
+        }
         data = data.results;
         var tableHeaders = getTableHeaders(data[0]);
 
@@ -146,6 +159,6 @@ function writeToDocument(type) {
             })
             tableRows.push(`<tr>${dataRow}</tr>`);
         });
-        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>`;
+        el.innerHTML = `<table>${tableHeaders}${tableRows}</table>${pagination}`;
     });
 }
